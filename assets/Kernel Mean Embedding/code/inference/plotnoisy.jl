b16e1fb377e6798d4311602ca7567a17c612f771
JasperDeLandsheere <68588995+JasperDeLandsheere@users.noreply.github.com>
2022-04-25 15:02:13 +0000

    Deploying to gh-pages from  @ bb033081b5f35cd52f16d823fe7b8ce094cd13f8 🚀

--- assets/Kernel Mean Embedding/code/inference/plotnoisy.jl
+++ assets/Kernel Mean Embedding/code/inference/plotnoisy.jl
@@ -1,5 +1,6 @@
 # This file was generated, do not modify it. # hide
-seed!(78)
+using Plots
+	seed!(78)
 	X = noisy_circle(100, 20, 1)
-	fignoisy = scatter(X[:,1], X[:,2])
-    savefig(joinpath(@OUTPUT, "fignoisy.svg")) # hide
+	scatter(X[:,1], X[:,2])
+    savefig(joinpath(@OUTPUT, "noisyfig.svg"))
