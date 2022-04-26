27f50fb6f1594fc28d3890896177bf33b29a8c0d
JasperDeLandsheere <68588995+JasperDeLandsheere@users.noreply.github.com>
2022-04-26 07:07:43 +0000

    Deploying to gh-pages from  @ e9ff2e682e90d74b9ada92a3f447abe245166929 🚀

--- assets/Kernel Mean Embedding/code/inference/using.jl
+++ assets/Kernel Mean Embedding/code/inference/using.jl
@@ -1,5 +1,10 @@
 # This file was generated, do not modify it. # hide
 import Pkg
 Pkg.add("Plots")
+Pkg.add("KernelFunctions")
+Pkg.add("LinearAlgebra")
+Pkg.add("StatsBase")
+Pkg.add("Distributions")
+Pkg.add("Random")
 using Plots, KernelFunctions, LinearAlgebra, StatsBase, Distributions
 using Random:seed!
